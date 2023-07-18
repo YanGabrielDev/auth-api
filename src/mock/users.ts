9a76af825a4a5a29235539103f4d52997ef2364a
yangabrp <yg@forpeople.io>
2023-07-18 17:01:54 -0300

    feat: create users mock

---
+++ src/mock/users.ts
@@ -0,0 +1,23 @@
+export const users = [
+  {
+    name: "Yan Gabriel",
+    email: "yangabriel@gmail.com",
+    password: "@28yanGabriel"
+  },
+  {
+    name: "Gabriel Fernandes",
+    email: "gabrielFernandes@gmail.com",
+    password: "@28gabrielFernandes"
+  },
+  {
+    name: "Pedro Henrique",
+    email: "pedroHenrique@gmail.com",
+    password: "@28pedroHenrique"
+  },
+  {
+    name: "Lucas inutilismo",
+    email: "lucasinutilismo@gmail.com",
+    password: "@28lucasInutilismo"
+  },
+  
+]
