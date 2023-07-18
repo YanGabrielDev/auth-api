275a8595b3b44b4c88fc8b97ff3428c36e602c8c
yangabrp <yg@forpeople.io>
2023-07-18 17:01:21 -0300

    feat: create user controller

---
+++ src/controllers/userControler.ts
@@ -0,0 +1,29 @@
+import {Request, Response} from 'express'
+import jwt from 'jsonwebtoken'
+import { users } from '../mock/users'
+
+export class UserController{
+  public signIn = (req: Request, res: Response) => {
+    const email = req.body.email
+    const password = req.body.password
+    const user = users.find((user) => user.email === email && user.password === password)
+
+    if(!user){
+     return res.status(400).send({error: "User not found!"});
+    }
+
+    const random = (length = 8) => {
+      const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let str = '';
+      for (let i = 0; i < length; i++) {
+        str += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
+      return str;
+    };
+    const token = jwt.sign(req.body, random(),{
+      expiresIn: '1 days'
+    })
+    
+    res.status(200).send({access_token: token})
+  }
+}
