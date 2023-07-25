4df05c94e9df69256941e12428678be7e19c722b
Yan <yangabriel2012@gmail.com>
2023-07-24 22:10:02 -0300

    fix: criando tabela user

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -1,25 +1,39 @@
-require('dotenv').config()
-const mysql = require('mysql2')
-const connection =  mysql.createConnection(process.env.DATABASE_URL)
-console.log(connection)
+const dotenv = require('dotenv');
+const mysql = require('mysql2/promise');
+
+dotenv.config();
+
+export async function connectToDatabase() {
+  try {
+    const connection = await mysql.createConnection(process.env.DATABASE_URL);
+    return connection;
+  } catch (error) {
+    console.error('Error connecting to the database:');
+    throw error;
+  }
+}
+
 async function createTable() {
+  const connection = await connectToDatabase();
   try {
     await connection.execute(`
-      CREATE TABLE plants (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      CREATE TABLE IF NOT EXISTS users (
+        id int NOT NULL AUTO_INCREMENT,
         name VARCHAR(255),
         image_url VARCHAR(255),
-        specie VARCHAR(255)
+        email VARCHAR(255),
+        password VARCHAR(255),
+        country VARCHAR(255),
+        PRIMARY KEY (id)
       )
     `);
 
     console.log('Table created successfully!');
   } catch (error) {
-    console.error(error);
+    console.error('Error creating table:');
   } finally {
     await connection.end();
   }
 }
 
 createTable();
-// connection.end()
